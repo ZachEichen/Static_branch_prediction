22449edaec052429b8cf7c69bf541d0e318f9724
Shin Lee <shinlee@umich.edu>
2024-04-12 11:34:44 -0400

    modify test.c

--- Loop/test.c
+++ Loop/test.c
@@ -1,9 +1,30 @@
+#include <stdio.h>
+
 int main() {
     
     int a = 0;
-    
-    for(int i=0; i<10; i++){
-        a++;
+    int b = 5;
+    int c = 3;
+    int d = 8;
+
+    int i = 0;
+
+    for(i = a; i<=b; i++){
+
+        int j = 0;
+
+        if(i==(a+b)/2) continue;
+
+        for(j = c; j < d; j++){
+
+            // Do stuff here
+            printf("Hello World!");
+
+            if(j % 2 == 0){
+                printf("This is an even number.");
+            }
+        }
+        
     }
 
     return 0;
